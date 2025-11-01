7c3f9f23dd1a8f13ef21687964fbba3f75779ffc
gabor-makeev <gabor.makejev@gmail.com>
2025-11-01 17:55:58 +0100

    implement function calling based on ai agent response provided

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from functions.get_files_info import schema_get_files_info
 from functions.get_file_content import schema_get_file_content
 from functions.run_python_file import schema_run_python_file
 from functions.write_file import schema_write_file
+from functions.call_function import call_function
 
 def main():
     args = sys.argv[1:]
@@ -32,54 +33,60 @@ def main():
     generate_response(client, user_prompt, verbose)
 
 def generate_response(client, user_prompt, verbose):
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-            schema_get_file_content,
-            schema_run_python_file,
-            schema_write_file
-        ]
-    )
+	available_functions = types.Tool(
+		function_declarations=[
+			schema_get_files_info,
+			schema_get_file_content,
+			schema_run_python_file,
+			schema_write_file
+		]
+	)
 
-    system_prompt = """
-    You are a helpful AI coding agent.
+	system_prompt = """
+	You are a helpful AI coding agent.
 
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+	When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
-    - List files and directories
-    - Read file contents
-    - Execute Python files with optional arguments
-    - Write or overwrite files
+	- List files and directories
+	- Read file contents
+	- Execute Python files with optional arguments
+	- Write or overwrite files
 
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-    """
+	All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+	"""
 
-    config=types.GenerateContentConfig(
-        tools=[available_functions], system_instruction=system_prompt
-    )
+	config=types.GenerateContentConfig(
+		tools=[available_functions], system_instruction=system_prompt
+	)
 
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-    ]
+	messages = [
+		types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+	]
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
-        contents=messages,
-        config=config
-    )
+	response = client.models.generate_content(
+		model="gemini-2.0-flash-001", 
+		contents=messages,
+		config=config
+	)
 
-    if verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
-    print("Response:")
+	if verbose:
+		print(f"User prompt: {user_prompt}")
+		print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+		print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+	
+	print("Response:")
 
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-    else:
-        print(response.text)
+	if response.function_calls:
+		for function_call_part in response.function_calls:
+			function_call_result = call_function(function_call_part, verbose)
+
+			if not function_call_result.parts[0].function_response.response:
+				raise Exception("Fatal Error: no function call result")
+					
+			if verbose:
+				print(f"-> {function_call_result.parts[0].function_response.response}")
+	else:
+		print(response.text)
 
 if __name__ == "__main__":
     main()
